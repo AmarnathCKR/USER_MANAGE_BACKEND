a8f59e10623d7175e6e7cebcab6529ee56db7796
AmarnathCKR <ckramarnath@gmail.com>
2023-11-11 11:41:05 +0530

    Fixing minor bugs

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: ["http://localhost:5173", "https://usermanagement-amarnathckr.netlify.app/"],
+        origin: ["https://usermanagement-amarnathckr.netlify.app","http://localhost:5173"],
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         credentials: true,
     })
